939904f65bbec5d648a6af48e8cee8ba66cabd14
Keith Moore <keith.m4@gmail.com>
2016-03-28 16:08:08 -0400

    Angularifying the gallery
    Refactoring the boostrap gallery with AngularJS

---
+++ controller.js
@@ -0,0 +1,42 @@
+var myApp = angular.module('myApp', []);
+myApp.controller('myController', function($scope){
+	$scope.images = [
+	{
+		source: "image1",
+		caption: "blah"
+	},
+	{
+		source: "image2",
+		caption: "blah"
+	},
+	{
+		source: "image3",
+		caption: "blah"
+	},
+	{
+		source: "image4",
+		caption: "blah"
+	},
+	{
+		source: "image5",
+		caption: "blah"
+	},
+	{
+		source: "image6",
+		caption: "blah"
+	},
+	{
+		source: "image7",
+		caption: "blah"
+	},
+	{
+		source: "image8",
+		caption: "blah"
+	},
+	{
+		source: "image9",
+		caption: "blah"
+	}
+	];
+
+})
